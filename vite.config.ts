0b50a8225e3d0ca6781f246fcc8232eb330e1408
Prath <askforvarn@gmail.com>
2026-02-03 20:17:25 +0530

    feat: add GitHub Actions workflow for deployment and configure base path in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      base: '/Legacy-of-Love_A-tribute-to-Dad/', // Add this line
     };
 });
